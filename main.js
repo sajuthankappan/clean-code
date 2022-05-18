bdc270cb710b4419ea77fb16465c355ff6705e62
Saju Thankappan <sajuthankappan@gmail.com>
2022-05-18 18:31:12 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Highligtht from 'reveal.js/plugin/highlight/highlight.esm'
+import Notes from 'reveal.js/plugin/notes/notes.esm';
+
+let deck = new Reveal({
+  plugins: [ Markdown, Highligtht, Notes ]
+})
+deck.initialize();
